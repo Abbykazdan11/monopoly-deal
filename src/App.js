3e22471656fbed89d2ef21f641d8750ee0b9774c
Abby Kazdan <abbykazdan@Abbys-MacBook-Pro.local>
2022-07-15 08:37:12 -0400

    add card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {Card, ListGroup} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card border="primary" style={{ width: '15rem' }}>
+        <Card.Body>
+          <Card.Title className="blue-background">Connecticut Avenue</Card.Title>
+          <Card.Text>
+            My Big Bam
+          </Card.Text>
+          <ListGroup variant="flush">
+            <ListGroup.Item>1 ... $1M</ListGroup.Item>
+            <ListGroup.Item>2 ... $2M</ListGroup.Item>
+            <ListGroup.Item>3 ... $3M</ListGroup.Item>
+          </ListGroup>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
