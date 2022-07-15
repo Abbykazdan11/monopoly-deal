9598a6c465eb64c78e9cb6d30d98ee7231d9d056
Jake Goodman <jakewgoodman@gmail.com>
2022-07-15 09:18:22 -0400

    add property component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
-import {Card, ListGroup} from 'react-bootstrap';
+import {Property} from './Property'
 
 function App() {
   return (
     <div className="App">
-      <Card border="primary" style={{ width: '15rem' }}>
-        <Card.Body>
-          <Card.Title className="blue-background">Connecticut Avenue</Card.Title>
-          <Card.Text>
-            My Big Bam
-          </Card.Text>
-          <ListGroup variant="flush">
-            <ListGroup.Item>1 ... $1M</ListGroup.Item>
-            <ListGroup.Item>2 ... $2M</ListGroup.Item>
-            <ListGroup.Item>3 ... $3M</ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
+      <Property
+        color="blue"
+        title="test title big bam"
+        rentValues={[1,2,3,4]}
+      />
     </div>
   );
 }
---
+++ src/Property.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Property.css';
+import {Card, ListGroup} from 'react-bootstrap';
+
+// props: {
+//   color: string;
+//   title: string;
+//   rentValues: number[]; 
+// }
+export function Property(props) {
+    const backgroundStyle = `${props.color}-background`;
+
+    const rentText = [];
+    for (let i = 1; i <= props.rentValues.length; i++) {
+        rentText.push(`${i} ... $${props.rentValues[i - 1]}M`);
+    }
+
+    return (
+      <Card border="primary" style={{ width: '15rem' }}>
+        <Card.Body>
+          <Card.Title className={backgroundStyle}>{props.title}</Card.Title>
+          <Card.Text>
+            My Big Bam
+          </Card.Text>
+          <ListGroup variant="flush">
+            {rentText.map((text) => {
+              return <ListGroup.Item>{text}</ListGroup.Item>
+            })}
+          </ListGroup>
+        </Card.Body>
+      </Card>
+    );
+}
